35454c9ea404b8bee15c91757b2ca4c2671837c6
koa <yl@koala.one>
2021-04-28 13:59:43 +0100

    youtube is working

--- src/components/youtubePlayer.svelte
+++ src/components/youtubePlayer.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { Player, Youtube} from "@vime/svelte";
+  import { Player, Youtube, DefaultUi} from "@vime/svelte";
   export let id;
 </script>
 <div class="container">
 <Player theme="dark" style="--vm-player-theme: #7F91EC;">
   <Youtube videoId="{id}" />
+  <DefaultUi />
 </Player>
 </div>
 
