bd8e15aa6b9528c8c96cbd243820baf9a986a090
koa <yl@koala.one>
2021-05-02 11:40:56 +0100

    fixed list caused error

--- src/components/dialog/list.svelte
+++ src/components/dialog/list.svelte
@@ -7,28 +7,30 @@
   export let open = false;
 </script>
 
-<Dialog
-  bind:open
-  selection
-  aria-labelledby="list-title"
-  aria-describedby="list-content"
->
-  <Title id="list-title">{$content.title}</Title>
-  <Content id="list-content">
-    <List>
-      {#each $episodes as { episode, season, episodeId, duration }}
-        <Item
-          on:SMUI:action={() => {
-            open = false;
-            $goto(`./watch?id=${episodeId}`);
-          }}
-        >
-          <Text>
-            <PrimaryText>Episode: {episode}</PrimaryText>
-            <SecondaryText>season: {season}</SecondaryText>
-          </Text>
-        </Item>
-      {/each}
-    </List>
-  </Content>
-</Dialog>
+{#if $episodes}
+  <Dialog
+    bind:open
+    selection
+    aria-labelledby="list-title"
+    aria-describedby="list-content"
+  >
+    <Title id="list-title">{$content.title}</Title>
+    <Content id="list-content">
+      <List>
+        {#each $episodes as { episode, season, episodeId, duration }}
+          <Item
+            on:SMUI:action={() => {
+              open = false;
+              $goto(`./watch?id=${episodeId}`);
+            }}
+          >
+            <Text>
+              <PrimaryText>Episode: {episode}</PrimaryText>
+              <SecondaryText>season: {season}</SecondaryText>
+            </Text>
+          </Item>
+        {/each}
+      </List>
+    </Content>
+  </Dialog>
+{/if}
