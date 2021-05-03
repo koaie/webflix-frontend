a19a727d063466cbea69426d4b87a99679e79cfb
koa <yl@koala.one>
2021-05-03 21:06:43 +0100

    fixed check

--- src/pages/checkout.svelte
+++ src/pages/checkout.svelte
@@ -10,74 +10,79 @@
   let errorText;
   let error;
   let body;
-  if ($user.id && !$user.paid) {
-    const call = async () => {
-      let result = await axios
-        .post(`${$API_URL}/auth/payment.php`, body, {
-          "Content-type": "application/json",
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      return result.data;
-    };
-
-    const paid = async () => {
-      body = JSON.stringify({
-        id: $user.id,
-      });
-      await call().then((res) => {
-        if (res.id) {
-          user.update((val) => {
-            val.paid = true;
-            return val;
+  if ($user.id) {
+    if ($user.paid) {
+      errorText = "You are already subscribed";
+      error = true;
+    } else {
+      const call = async () => {
+        let result = await axios
+          .post(`${$API_URL}/auth/payment.php`, body, {
+            "Content-type": "application/json",
+          })
+          .catch((err) => {
+            console.log(err);
           });
-          $goto("./shows");
-        }
-        if (res.error) {
-          errorText = res.error;
-          error = true;
-        }
-      });
-    };
+        return result.data;
+      };
 
-    const CLIENT_ID =
-      "ASo51rJYa2auckprmMGuI5693w5NgojJ8LzV7A2-8m_sSBYO8tY9DBbqJbJZ3PJqu0ewvcsBh6qsowTv";
-    loadScript({ "client-id": CLIENT_ID, currency: "GBP" }).then((paypal) => {
-      paypal
-        .Buttons({
-          style: {
-            color: "blue",
-            shape: "pill",
-          },
-          createOrder: function (data, actions) {
-            // Set up the transaction
-            return actions.order.create({
-              purchase_units: [
-                {
-                  amount: {
-                    currency_code: "GBP",
-                    value: "99",
-                  },
-                  description: "Webflix Premium",
-                  custom_id: "64735",
-                },
-              ],
+      const paid = async () => {
+        body = JSON.stringify({
+          id: $user.id,
+        });
+        await call().then((res) => {
+          if (res.id) {
+            user.update((val) => {
+              val.paid = true;
+              return val;
             });
-          },
-          onApprove: function (data, actions) {
-            // Capture order after payment approved
-            return actions.order.capture().then(function (details) {
-              paid();
-            });
-          },
-          onError: function (err) {
-            errorText = err;
+            $goto("./shows");
+          }
+          if (res.error) {
+            errorText = res.error;
             error = true;
-          },
-        })
-        .render("#paypal-button-container");
-    });
+          }
+        });
+      };
+
+      const CLIENT_ID =
+        "ASo51rJYa2auckprmMGuI5693w5NgojJ8LzV7A2-8m_sSBYO8tY9DBbqJbJZ3PJqu0ewvcsBh6qsowTv";
+      loadScript({ "client-id": CLIENT_ID, currency: "GBP" }).then((paypal) => {
+        paypal
+          .Buttons({
+            style: {
+              color: "blue",
+              shape: "pill",
+            },
+            createOrder: function (data, actions) {
+              // Set up the transaction
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: "GBP",
+                      value: "99",
+                    },
+                    description: "Webflix Premium",
+                    custom_id: "64735",
+                  },
+                ],
+              });
+            },
+            onApprove: function (data, actions) {
+              // Capture order after payment approved
+              return actions.order.capture().then(function (details) {
+                paid();
+              });
+            },
+            onError: function (err) {
+              errorText = err;
+              error = true;
+            },
+          })
+          .render("#paypal-button-container");
+      });
+    }
   }
 </script>
 
@@ -93,11 +98,7 @@
 {/if}
 
 {#if $user.id}
-  {#if $user.paid}
-    errorText = "You are already subscribed"; error = true;
-  {:else}
-    <div id="paypal-button-container" />
-  {/if}
+  <div id="paypal-button-container" />
 {:else}
   <div class="container">
     <ActionCard
