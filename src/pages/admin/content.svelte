f22184ddc2f0332109bdf336c14c2c0e681aa697
koa <yl@koala.one>
2021-05-14 03:49:29 +0100

    edit working

--- src/pages/admin/content.svelte
+++ src/pages/admin/content.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { API_URL, user } from "../../logic/stores";
+  import { API_URL, content, user } from "../../logic/stores";
   import { goto } from "@roxi/routify";
   import axios from "axios";
 
@@ -10,17 +10,25 @@
   import { Content } from "@smui/dialog";
   import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
   import Select, { Option } from "@smui/select";
+  import Button, { Label as ButLabel } from "@smui/button";
+
+  import LayoutGrid, { Cell as GridCell } from "@smui/layout-grid";
 
   import Textfield from "@smui/textfield";
-  import Icon from "@smui/textfield/icon";
 
   import ActionCard from "../../components/card/action.svelte";
   import Dialog from "../../components/dialog/form.svelte";
 
   import { clickOutside } from "../../components/layout/Outside.js";
 
+  let title = null;
+  let desc = null;
+  let cover = null;
+  let language = null;
+  let trailer = null;
   let date = null;
   let email = null;
+  let type = null;
   let invalid = false;
   const dateRegex =
     /(201[0-7]|200[0-9]|[0-1][0-9]{3})[-.\/](1[0-2]|0[1-9])[-.\/](3[01]|[0-2][1-9]|[12]0)/;
@@ -28,6 +36,8 @@
   let invalidDate = false;
   let number = null;
   let dropbox = null;
+  let selected = [];
+  let mode = true;
 
   let data;
   const _seasons = new Map();
@@ -43,15 +53,27 @@
   let viewMenu;
   let seasonMenu;
   let editMenu;
-  let editEpisodeMenu;
   let deleteEpisode;
   let deleteSeason;
   let showEpisode = false;
   let showSeason = false;
   let showEditEpisode = false;
-
+  let contentMenu = false;
   let error;
   let errorText;
+
+  $: {
+    if (mode) {
+      title = null;
+      desc = null;
+      cover = null;
+      language = null;
+      trailer = null;
+      date = null;
+      email = null;
+      type = null;
+    }
+  }
 
   $: {
     if (_content) {
@@ -176,12 +198,14 @@
     return result.data;
   };
 
-  const link = async () => {
+  const link = async (contentId, genreId) => {
     let result = await axios
       .post(
-        `${$API_URL}/content/view.php`,
+        `${$API_URL}/admin/genre/link.php`,
         JSON.stringify({
           id: $user.id,
+          genreId: genreId,
+          contentId: contentId,
         }),
         {
           "Content-type": "application/json",
@@ -190,23 +214,7 @@
       .catch((err) => {
         console.log(err);
       });
-    return result.data;
-  };
-
-  const unlink = async () => {
-    let result = await axios
-      .post(
-        `${$API_URL}/content/view.php`,
-        JSON.stringify({
-          id: $user.id,
-        }),
-        {
-          "Content-type": "application/json",
-        }
-      )
-      .catch((err) => {
-        console.log(err);
-      });
+    view();
     return result.data;
   };
 
@@ -324,15 +332,33 @@
       .catch((err) => {
         console.log(err);
       });
-    console.log(result.data);
     setEpisodes(result.data);
     return result.data;
   };
 
-  const addContent = async () => {
+  const addType = async (contentId, type) => {
     let result = await axios
       .post(
-        `${$API_URL}/content/view.php`,
+        `${$API_URL}/admin/content/type/add.php`,
+        JSON.stringify({
+          id: $user.id,
+          contentId: contentId,
+          type: type,
+        }),
+        {
+          "Content-type": "application/json",
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+    return result;
+  };
+
+  const viewGenres = async () => {
+    let result = await axios
+      .post(
+        `${$API_URL}/genre/view.php`,
         JSON.stringify({
           id: $user.id,
         }),
@@ -343,6 +369,57 @@
       .catch((err) => {
         console.log(err);
       });
+    return result.data;
+  };
+
+  const addContent = async () => {
+    let result = await axios
+      .post(
+        `${$API_URL}/admin/content/add.php`,
+        JSON.stringify({
+          id: $user.id,
+          title: title,
+          cover: cover,
+          desc: desc,
+          release: date,
+          duration: 1,
+          language: language,
+          trailer: trailer,
+        }),
+        {
+          "Content-type": "application/json",
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+    view();
+    return result.data;
+  };
+
+  const editContent = async () => {
+    let result = await axios
+      .post(
+        `${$API_URL}/admin/content/edit.php`,
+        JSON.stringify({
+          id: $user.id,
+          contentId: _content.contentId,
+          title: title ? title : _content.title,
+          cover: cover ? cover : _content.cover,
+          desc: desc ? desc : _content.desc,
+          release: date ? date : _content.release,
+          duration: 1,
+          language: language ? language : _content.language,
+          trailer: trailer ? trailer : _content.trailer,
+        }),
+        {
+          "Content-type": "application/json",
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+    view();
     return result.data;
   };
 
@@ -426,6 +503,53 @@
 </script>
 
 <Dialog
+  title="Add"
+  open={contentMenu}
+  on:click={async () => {
+    if (title && desc && trailer && !invalidDate && language && cover && type) {
+      let content = await addContent();
+      await addType(content.contentId, type);
+      selected.forEach(async (el) => {
+        await link(content.contentId, el.genreId);
+      });
+    }
+    contentMenu = false;
+  }}
+  buttonText="Add"
+>
+  <class id="form">
+    <LayoutGrid>
+      <GridCell><Textfield bind:value={title} label="Title" /></GridCell>
+      <GridCell><Textfield bind:value={desc} label="Description" /></GridCell>
+      <GridCell><Textfield bind:value={cover} label="Cover" /></GridCell>
+      <GridCell
+        ><Textfield
+          bind:value={date}
+          bind:invalid={invalidDate}
+          label="Released"
+        /></GridCell
+      >
+      <GridCell><Textfield bind:value={language} label="Language" /></GridCell>
+      <GridCell><Textfield bind:value={trailer} label="Trailer ID" /></GridCell>
+      <GridCell>
+        <Select bind:value={type} label="Type">
+          {#each ["TV show", "Movie"] as type}
+            <Option value={type}>{type}</Option>
+          {/each}
+        </Select></GridCell
+      >
+    </LayoutGrid>
+    {#await viewGenres() then genres}
+      <ChipSet chips={genres} let:chip filter bind:selected>
+        <Chip {chip} touch>
+          <Text>{chip.name}</Text>
+        </Chip>
+      </ChipSet>
+    {/await}
+  </class>
+</Dialog>
+
+<Dialog
   title="Edit"
   open={seasonMenu}
   on:click={() => {
@@ -487,164 +611,197 @@
   title="Edit"
   open={editMenu}
   on:click={() => {
-    if (_content) {
+    if (_content && mode) {
       edit(_content);
+    } else if (_content && !mode) {
+      editContent();
     }
     editMenu = false;
   }}
   buttonText="Save"
 >
   {#if _content}
-    {#each seasons ?? [] as season}
-      <div class="con">
-        <div class="left">
-          Season {season.number}
-        </div>
-        <div class="item right">
-          <IconButton
-            class="material-icons"
-            on:click={() => {
-              showEpisode = !showEpisode;
-              path = "";
-            }}>add</IconButton
-          >
-          <IconButton
-            class="material-icons"
-            on:click={() => {
-              showSeason = !showSeason;
-            }}>edit</IconButton
-          >
-          <IconButton
-            class="material-icons"
-            on:click={() => {
-              deleteSeason = season;
-              delSeason(season.seasonId);
-            }}>delete</IconButton
-          >
-        </div>
-      </div>
-      {#if showSeason}
-        <div
-          class="con"
-          use:clickOutside
-          on:click_outside={() => (showSeason = false)}
-        >
+    {#if mode}
+      {#each seasons ?? [] as season}
+        <div class="con">
           <div class="left">
-            <Textfield bind:value={number} label="Season number" />
+            Season {season.number}
           </div>
-          <div class="right">
-            <div class="botMargin">
-              <IconButton
-                class="material-icons"
-                on:click={() => {
-                  editSeason(season);
-                }}>save</IconButton
-              >
-            </div>
-          </div>
-        </div>
-      {/if}
-      {#if showEpisode}
-        <div use:clickOutside on:click_outside={() => (showEpisode = false)}>
-          <div class="top">
-            <Textfield bind:value={number} label="Episode number" />
-          </div>
-          <div class="bot">
-            <Textfield bind:value={path} label="Url" />
-          </div>
-          <div class="right">
-            <div class="botMarginRows">
-              <IconButton
-                class="material-icons"
-                on:click={() => {
-                  addEpisode(season.contentId, season.seasonId, path, number);
-                }}>add</IconButton
-              >
-            </div>
-          </div>
-        </div>
-      {/if}
-      <div class="container" id="contentEdit">
-        <DataTable style="max-width: 100%;">
-          <Head>
-            <Row>
-              <Cell>Episode</Cell>
-              <Cell>Edit</Cell>
-              <Cell>Remove</Cell>
-            </Row>
-          </Head>
-          <Body>
-            {#each _episodes as content}
-              {#if content.seasonId === season.seasonId}
-                <Row>
-                  <Cell>{content.episode}</Cell>
-                  <Cell>
-                    <IconButton
-                      class="material-icons"
-                      on:click={() => {
-                        _episode = content;
-                        path = "";
-                        showEditEpisode = !showEditEpisode;
-                      }}>edit</IconButton
-                    >
-                  </Cell>
-                  <Cell>
-                    <IconButton
-                      class="material-icons"
-                      on:click={() => {
-                        deleteEpisode = content;
-                        delEpisode(content.episodeId);
-                      }}>delete</IconButton
-                    ></Cell
-                  >
-                </Row>
-              {/if}
-            {/each}
-          </Body>
-          {#if showEditEpisode}
-            <div
-              use:clickOutside
-              on:click_outside={() => {
-                showEditEpisode = false;
+          <div class="item right">
+            <IconButton
+              class="material-icons"
+              on:click={() => {
+                showEpisode = !showEpisode;
                 path = "";
-              }}
+              }}>add</IconButton
             >
-              <div class="top">
-                <Textfield bind:value={number} label="Episode number" />
+            <IconButton
+              class="material-icons"
+              on:click={() => {
+                showSeason = !showSeason;
+              }}>edit</IconButton
+            >
+            <IconButton
+              class="material-icons"
+              on:click={() => {
+                deleteSeason = season;
+                delSeason(season.seasonId);
+              }}>delete</IconButton
+            >
+          </div>
+        </div>
+        {#if showSeason}
+          <div
+            class="con"
+            use:clickOutside
+            on:click_outside={() => (showSeason = false)}
+          >
+            <div class="left">
+              <Textfield bind:value={number} label="Season number" />
+            </div>
+            <div class="right">
+              <div class="botMargin">
+                <IconButton
+                  class="material-icons"
+                  on:click={() => {
+                    editSeason(season);
+                  }}>save</IconButton
+                >
               </div>
-              <div class="bot">
-                <Textfield bind:value={path} label="Url" />
+            </div>
+          </div>
+        {/if}
+        {#if showEpisode}
+          <div use:clickOutside on:click_outside={() => (showEpisode = false)}>
+            <div class="top">
+              <Textfield bind:value={number} label="Episode number" />
+            </div>
+            <div class="bot">
+              <Textfield bind:value={path} label="Url" />
+            </div>
+            <div class="right">
+              <div class="botMarginRows">
+                <IconButton
+                  class="material-icons"
+                  on:click={() => {
+                    addEpisode(season.contentId, season.seasonId, path, number);
+                  }}>add</IconButton
+                >
               </div>
-              <div class="right">
-                <div class="botMarginRows">
-                  <IconButton
-                    class="material-icons"
-                    on:click={() => {
-                      if (path) {
-                        editEpisode(season.contentId, _episode, number, path);
-                      }
-                    }}>save</IconButton
-                  >
+            </div>
+          </div>
+        {/if}
+        <div class="container" id="contentEdit">
+          <DataTable style="max-width: 100%;">
+            <Head>
+              <Row>
+                <Cell>Episode</Cell>
+                <Cell>Edit</Cell>
+                <Cell>Remove</Cell>
+              </Row>
+            </Head>
+            <Body>
+              {#each _episodes as content}
+                {#if content.seasonId === season.seasonId}
+                  <Row>
+                    <Cell>{content.episode}</Cell>
+                    <Cell>
+                      <IconButton
+                        class="material-icons"
+                        on:click={() => {
+                          _episode = content;
+                          path = "";
+                          showEditEpisode = !showEditEpisode;
+                        }}>edit</IconButton
+                      >
+                    </Cell>
+                    <Cell>
+                      <IconButton
+                        class="material-icons"
+                        on:click={() => {
+                          deleteEpisode = content;
+                          delEpisode(content.episodeId);
+                        }}>delete</IconButton
+                      ></Cell
+                    >
+                  </Row>
+                {/if}
+              {/each}
+            </Body>
+            {#if showEditEpisode}
+              <div
+                use:clickOutside
+                on:click_outside={() => {
+                  showEditEpisode = false;
+                  path = "";
+                }}
+              >
+                <div class="top">
+                  <Textfield bind:value={number} label="Episode number" />
+                </div>
+                <div class="bot">
+                  <Textfield bind:value={path} label="Url" />
+                </div>
+                <div class="right">
+                  <div class="botMarginRows">
+                    <IconButton
+                      class="material-icons"
+                      on:click={() => {
+                        if (path) {
+                          editEpisode(season.contentId, _episode, number, path);
+                        }
+                      }}>save</IconButton
+                    >
+                  </div>
                 </div>
               </div>
-            </div>
-          {/if}
-        </DataTable>
+            {/if}
+          </DataTable>
+        </div>
+      {/each}
+      <div class="right">
+        <div class="width">
+          <Textfield bind:value={number} label="Number" />
+        </div>
+        <IconButton
+          class="material-icons"
+          on:click={() => {
+            if (number > 0) {
+              addSeason(_content.contentId, number);
+            }
+          }}>add</IconButton
+        >
       </div>
-    {/each}
-    <div class="right">
-      <div class="width">
-        <Textfield bind:value={number} label="Number" />
+    {:else}
+      <div id="form">
+        <LayoutGrid>
+          <GridCell><Textfield bind:value={title} label="Title" /></GridCell>
+          <GridCell
+            ><Textfield bind:value={desc} label="Description" /></GridCell
+          >
+          <GridCell><Textfield bind:value={cover} label="Cover" /></GridCell>
+          <GridCell
+            ><Textfield
+              bind:value={date}
+              bind:invalid={invalidDate}
+              label="Released"
+            /></GridCell
+          >
+          <GridCell
+            ><Textfield bind:value={language} label="Language" /></GridCell
+          >
+          <GridCell
+            ><Textfield bind:value={trailer} label="Trailer ID" /></GridCell
+          >
+        </LayoutGrid>
       </div>
-      <IconButton
-        class="material-icons"
-        on:click={() => {
-          if (number > 0) {
-            addSeason(_content.contentId, number);
-          }
-        }}>add</IconButton
-      >
-    </div>
+    {/if}
+    <IconButton
+      class="material-icons"
+      on:click={() => {
+        mode = !mode;
+      }}>developer_mode</IconButton
+    >
   {:else if !$user}
     <div class="card-container">
       <ActionCard
@@ -717,7 +874,13 @@
       </DataTable>
     </div>
     <div class="right">
-      <IconButton class="material-icons" on:click={() => {}}>add</IconButton>
+      <IconButton
+        class="material-icons"
+        on:click={() => {
+          contentMenu = false;
+          contentMenu = true;
+        }}>add</IconButton
+      >
     </div>
   {:else if !$user}
     <div class="card-container">
